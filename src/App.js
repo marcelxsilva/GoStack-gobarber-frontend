138b50ce3d55f2440d8280792b67aaaa68097dcb
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-19 14:55:20 -0300

    define route, config history and create some pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import { Router } from 'react-router-dom'
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  return <Router history={history}><Routes /></Router>
 }
-
 export default App;
