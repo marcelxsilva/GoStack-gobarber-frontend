d450bfb0f24394fc9428dd14856ca12e18a4bdee
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-19 21:16:17 -0300

    aplly style in page signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg'
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <div />
+    <>
+      <img src={logo} alt='logo' />
+      <form>
+        <input placeholder='Nome Completo' />
+        <input type='email' placeholder='E-mail' />
+        <input type='password' placeholder='Senha' />
+        <button type='submit'>Acessar</button>
+        <Link to='/'>Acessar Minha Conta</Link>
+      </form>
+    </>
   );
 }
