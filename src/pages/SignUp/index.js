27583634e924da00e08a45a5971653e13e98ef9c
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-19 21:29:59 -0300

    use unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '~/assets/logo.svg'
+import { Input, Form } from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignUp() {
+
+  function handleSubmit(data){
+    console.tron.log(data)
+  }
   return (
     <>
       <img src={logo} alt='logo' />
-      <form>
-        <input placeholder='Nome Completo' />
-        <input type='email' placeholder='E-mail' />
-        <input type='password' placeholder='Senha' />
+      <Form onSubmit={handleSubmit}>
+        <Input name='name' placeholder='Nome Completo' />
+        <Input name='email' type='email' placeholder='E-mail' />
+        <Input name='password' type='password' placeholder='Senha' />
         <button type='submit'>Acessar</button>
         <Link to='/'>Acessar Minha Conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
