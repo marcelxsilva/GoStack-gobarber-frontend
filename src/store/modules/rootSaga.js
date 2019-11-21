2364b32371390a668b8cf0b42aafdb5576b7f273
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-21 11:14:38 -0300

    fix bug in file

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,12 @@
 import { all } from 'redux-saga/effects';
 
-import auth from './auth/reducer';
+// all sagas
+import auth from './auth/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth,])
+  return yield all(
+    [
+      auth,
+    ]
+  )
 }
