dbed14d2bc8bc7bbf6836890e9ce4d51ee2c0f4d
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-21 12:10:39 -0300

    created reducer to store data user

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -2,11 +2,13 @@ import { all } from 'redux-saga/effects';
 
 // all sagas
 import auth from './auth/sagas';
+import user from './user/sagas';
 
 export default function* rootSaga() {
   return yield all(
     [
       auth,
+      user,
     ]
   )
 }
