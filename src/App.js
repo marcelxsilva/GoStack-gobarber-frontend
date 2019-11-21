2c2039c30abe82ca41fdf583e5a3aa51abbf877e
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-21 09:36:33 -0300

    set redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Router } from 'react-router-dom'
 import Routes from './routes';
+import { Provider, } from 'react-redux';
 import history from './services/history';
 import './config/ReactotronConfig';
+
 import GlobalStyles from './styles/global';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   )
 }
 export default App;
