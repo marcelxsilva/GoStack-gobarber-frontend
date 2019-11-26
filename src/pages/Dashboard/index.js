75848dd38a28a1eda28ca688cdc82280d05c6197
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-26 11:59:16 -0300

    apply style in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-import api from '~/services/api'; 
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments')
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <header>
+        <button type='button' ><MdChevronLeft size={36} color='#fff' /> </button>
+        <strong>31 de Maio</strong>
+        <button type='button' ><MdChevronRight size={36} color='#fff' /> </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Marcelo Silva</span>
+        </Time>
+
+        <Time avaliable>
+          <strong>09:00</strong>
+          <span>Horario Disponivel</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
