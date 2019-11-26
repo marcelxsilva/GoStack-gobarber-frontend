6b2d5507c515c53fb8b11bdb4f6fb2f49e255915
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-26 10:03:09 -0300

    set style in page profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+  console.log(profile)
+
+  function handleSubmit(){
+
+  }
+
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name='name' placeholder='Nome Completo' />
+        <Input name='email' placeholder='E-Mail' />
+        <hr />
+        <Input type='password' name='oldPassword' placeholder='Senha Atual' />
+        <Input type='password' name='password' placeholder='Nova Senha' />
+        <Input type='password' name='confirmPassword' placeholder='Confirmar Senha' />
+        <button type='submit'>Atualizar Perfil</button>
+      </Form>
+      <button type='submit'>Sair do GoBarber</button>
+    </Container>
   );
 }
